cc9d5ebc884893f533acd8c258f37cc8f8b46b21
orlandin123 <orlandonf.ti20@utsjr.edu.mx>
2024-03-22 17:08:48 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './pages/menu/menu.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
